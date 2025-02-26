a1a5f804e3573f1a2d79ccc4118afd6500e0f88d
Akash.A <113507265+Akashadd@users.noreply.github.com>
2025-02-26 11:01:19 +0530

    Add files via upload

---
+++ unit2_unique_ele_1.R
@@ -0,0 +1,7 @@
+string_data <- c("apple", "banana", "apple", "orange", "banana")
+unique_string <- unique(string_data)
+print(unique_string)
+
+vector_data <- c(1, 2, 2, 3, 4, 4, 5)
+unique_vector <- unique(vector_data)
+print(unique_vector)
